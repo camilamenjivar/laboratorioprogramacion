eadd7dbc1951d2ce46ec0bd3c9649a4016476562
Camila Alessandra Menjivar Martinez <00095825@uca.edu.sv>
2025-05-30 15:52:26 -0600

    Add files via upload

---
+++ for.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int numero;
+    cout << "ingrese un numero";
+    cin>> numero;
+
+    if (numero < 0){
+        cout << "No puedes ingresar un numero menor a 0" <<endl;
+
+    }
+    else {
+        cout << "Tablas de multiplicar del " << numero << ":" <<endl;
+        for (int i = 1; i < 10; i++){
+            cout << numero << "x" << i << "=" << numero * i <<endl;
+        }
+        }
+    
+    return 0;
+}
